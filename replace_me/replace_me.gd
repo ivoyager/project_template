44fddd9a6e47f4ac3f8f2b4cb2f8e70a8045f93d
Charlie Whitfield <charliewhitfield@zoho.com>
2019-12-12 10:30:53 -0500

    Project file changes

--- replace_me/replace_me.gd
+++ replace_me/replace_me.gd
@@ -1,4 +1,19 @@
 # replace_me.gd
+# This file is part of I, Voyager
+# https://ivoyager.dev
+# Copyright (c) 2017-2019 Charlie Whitfield
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # *****************************************************************************
 # This is an example "extension" file that changes some init values and shows
 # how to extend an existing class or add a new class (these are commented out).
@@ -44,7 +59,7 @@ func _on_project_objects_instantiated() -> void:
 	var settings_manager: SettingsManager = Global.objects.SettingsManager
 	settings_manager.defaults.save_base_name = "Template"
 
-func _on_gui_entered_tree(gui_panel: Control) -> void:
+func _on_gui_entered_tree(_gui_panel: Control) -> void:
 	# Here you can access and change init values for GUI classes not defined in
 	# ProjectBuilder (i.e., SelectionPanel & NavigationPanel) before
 	# they are added to the tree.
