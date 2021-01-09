62f3e94ed8291b22522682be6eb9c2842aea222d
Charlie Whitfield <charliewhitfield@zoho.com>
2021-01-09 16:09:36 -0500

    Added ModDynamic (WIP - SysNav hookup)

--- replace_me/example_gui/select_panel.gd
+++ replace_me/example_gui/select_panel.gd
@@ -15,31 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # *****************************************************************************
-# You can modify widgets or set widget init values in _ready() here.
+# You can modify widget functionality, including changing widget init values,
+# in _ready() here.
 
 extends PanelContainer
 
-var default_sizes := [
-	Vector2(435.0, 291.0), # GUI_SMALL
-	Vector2(575.0, 354.0), # GUI_MEDIUM
-	Vector2(712.0, 421.0), # GUI_LARGE
-]
-
 func _ready() -> void:
-	Global.connect("gui_refresh_requested", self, "_resize")
-	Global.connect("setting_changed", self, "_settings_listener")
-	# widget mods here
-	$VBox/BottomHBox/ViewButtons/Outward.hide()
-
-func _resize() -> void:
-	# assumes anchor_left == anchor_right and anchor_top == anchor_bottom
-	var gui_size: int = Global.settings.gui_size
-	var viewport := get_viewport()
-	rect_size = default_sizes[gui_size]
-	rect_position.x = anchor_left * (viewport.size.x - rect_size.x)
-	rect_position.y = anchor_top * (viewport.size.y - rect_size.y)
-
-func _settings_listener(setting: String, _value) -> void:
-	match setting:
-		"gui_size":
-			_resize()
+	$VBox/BottomHBox/ViewButtons/Outward.hide() # under dev for astronomy in the Planetarium
