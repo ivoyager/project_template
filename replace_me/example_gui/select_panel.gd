0b917744f5889fc724def38d21e7be0edea7d552
Charlie Whitfield <charliewhitfield@zoho.com>
2021-01-09 08:19:53 -0500

    Improvements to panel sizing

--- replace_me/example_gui/select_panel.gd
+++ replace_me/example_gui/select_panel.gd
@@ -19,31 +19,27 @@
 
 extends PanelContainer
 
-var content_sizes := [
-	Vector2(421.0, 277.0), # GUI_SMALL
-	Vector2(561.0, 340.0), # GUI_MEDIUM
-	Vector2(698.0, 407.0), # GUI_LARGE
+var default_sizes := [
+	Vector2(435.0, 291.0), # GUI_SMALL
+	Vector2(575.0, 354.0), # GUI_MEDIUM
+	Vector2(712.0, 421.0), # GUI_LARGE
 ]
-var v_spacer_sizes := [3.0, 4.0, 5.0]
-var horizontal_separations := [6.0, 8.0, 10.0]
 
 func _ready() -> void:
-	Global.connect("gui_refresh_requested", self, "_resize_to_corner")
+	Global.connect("gui_refresh_requested", self, "_resize")
 	Global.connect("setting_changed", self, "_settings_listener")
 	# widget mods here
 	$VBox/BottomHBox/ViewButtons/Outward.hide()
 
-func _resize_to_corner() -> void:
+func _resize() -> void:
+	# assumes anchor_left == anchor_right and anchor_top == anchor_bottom
 	var gui_size: int = Global.settings.gui_size
-	$VBox.rect_min_size = content_sizes[gui_size]
-	$VBox/VSpacer.rect_min_size.y = v_spacer_sizes[gui_size]
-	$VBox/MainHBox.set("custom_constants/separation", horizontal_separations[gui_size])
-	yield(get_tree(), "idle_frame") # wait for content to resize
-	set_anchors_and_margins_preset(PRESET_BOTTOM_LEFT, PRESET_MODE_MINSIZE)
-	yield(get_tree(), "idle_frame")
-	print("SelectPanel size: ", rect_size)
+	var viewport := get_viewport()
+	rect_size = default_sizes[gui_size]
+	rect_position.x = anchor_left * (viewport.size.x - rect_size.x)
+	rect_position.y = anchor_top * (viewport.size.y - rect_size.y)
 
 func _settings_listener(setting: String, _value) -> void:
 	match setting:
 		"gui_size":
-			_resize_to_corner()
+			_resize()
